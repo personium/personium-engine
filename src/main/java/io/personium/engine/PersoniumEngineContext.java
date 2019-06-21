735018d80502bcdcc1b4950e2e3c447e83482b6e
Tochiori, Yasufumi <tochiori.y@jp.fujitsu.com>
2019-06-21 12:56:00 +0900

    Correct to close properly

--- src/main/java/io/personium/engine/PersoniumEngineContext.java
+++ src/main/java/io/personium/engine/PersoniumEngineContext.java
@@ -478,6 +478,7 @@ public class PersoniumEngineContext implements Closeable {
                 jsBuildObject = cx.compileReader(isr, path.getPath(), 1, null);
             } finally {
                 IOUtils.closeQuietly(isr);
+                IOUtils.closeQuietly(fis);
             }
             engineLibCache.put(path.toString(), jsBuildObject);
         }
